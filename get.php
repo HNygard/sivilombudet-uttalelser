292d91fae2b1adf760eba33f351fb5800749320f
Hallvard Nygard <hallvard.nygard@gmail.com>
2023-10-20 21:04:19 +0200

    PHP 8

--- get.php
+++ get.php
@@ -7,7 +7,7 @@
  * @author Hallvard Nygård, @hallny
  */
 
-set_error_handler(function ($errno, $errstr, $errfile, $errline, array $errcontext) {
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
     throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
 });
 
@@ -577,18 +577,6 @@ function getUrlCachedUsingCurl($cacheTimeSeconds, $cache_file, $baseUri, $accept
     return $body;
 }
 
-function str_starts_with($haystack, $needle) {
-    return substr($haystack, 0, strlen($needle)) == $needle;
-}
-
-function str_ends_with($haystack, $needle) {
-    $length = strlen($needle);
-    return $length === 0 || substr($haystack, -$length) === $needle;
-}
-
-function str_contains($stack, $needle) {
-    return (strpos($stack, $needle) !== FALSE);
-}
 
 function logDebug($string) {
     //logLine($string, 'DEBUG');
